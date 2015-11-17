c2af0603238ff1d205a3416efc13b653f1cab81d
nsrchemie <nsrchemie@gmail.com>
2015-11-17 10:22:55 -0800

    html func_tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -9,6 +9,7 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):  #1
@@ -27,10 +28,27 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  #5
-        self.fail('Finish the test!')  #6
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        # [...rest of comments as before]
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item')
 
+        inputbox.send_keys('Buy peacock feathers')
+
+
+        inputbox.send_keys(Keys.ENTER)
+
+
+        table = self.browser,find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 if __name__ == '__main__':  #7
     unittest.main(warnings='ignore')  #8
