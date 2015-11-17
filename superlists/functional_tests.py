82dcd593fb20c3c2efdeafbd05f4c8d1f8849bc2
nsrchemie <nsrchemie@gmail.com>
2015-11-17 11:45:52 -0800

    Expanded html and tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -43,10 +43,11 @@ class NewVisitorTest(unittest.TestCase):  #1
         inputbox.send_keys(Keys.ENTER)
 
 
-        table = self.browser,find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New To-Do item did not appear in table"
         )
 
         self.fail('Finish the test!')
