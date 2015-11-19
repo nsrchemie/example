7c632bb50127f6fdc9e280bdb755492b07defd32
nsrchemie <nsrchemie@gmail.com>
2015-11-18 16:11:53 -0800

    add helper method to functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -21,6 +21,12 @@ class NewVisitorTest(unittest.TestCase):  #1
     def tearDown(self):  #3
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -33,37 +39,29 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+        'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
-
-
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 
-                # There is still a text box inviting her to add another item. She
+
+
+
+        # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
-
-
-
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test!')
 if __name__ == '__main__':  #7
     unittest.main(warnings='ignore')  #8
