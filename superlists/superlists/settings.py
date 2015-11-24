4982d56b17d974c6dc04a798187cba8142f047f1
nsrchemie <nsrchemie@gmail.com>
2015-11-24 10:08:20 -0800

    migrations

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'superdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Memoria',
+        'HOST': 'localhost',
+        'PORT': '5432',
 }
-
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
