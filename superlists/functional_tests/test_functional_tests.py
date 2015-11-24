f91d126b5d19640e693f5c1f19dc9afc51694468
nsrchemie <nsrchemie@gmail.com>
2015-11-24 14:55:36 -0800

    New URL, view and template to display list and add second user to functest

--- superlists/functional_tests/test_functional_tests.py
+++ superlists/functional_tests/test_functional_tests.py
@@ -44,7 +44,11 @@ class NewVisitorTest(LiveServerTestCase):  #1
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+
 
 
 
@@ -62,6 +66,46 @@ class NewVisitorTest(LiveServerTestCase):  #1
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
+        #Now a new user comes along to the site
+
+        #We use a new browser session to make sure that no information of ediths is coming through from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        #Francis visits the homepage, there is no sign of ediths list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        #Francis starts a new list by entering a new item
+        #He is less interesting than Edith
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotIn(francis_list_url, edith_list_url)
+
+        #Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk, page_text')
+
+        #Satisfied, they both go to sleep
+
+
+
         self.fail('Finish the test!')
+
+
+
+
+
+
+
 # if __name__ == '__main__':  #7
 #     unittest.main(warnings='ignore')  #8
